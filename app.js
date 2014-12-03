7d6d22c79d12bb7bbff51145de589ef83b403ee1
Anup <anup@comitydesigns.com>
2014-12-03 14:34:16 -0800

    added callback

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/canvas/callback/',function(req,res){
 
 app.post('/canvas/callback',function(req,res){
 
-    console.log(res);
+    res.redirect('http://google.com');
 })
 
 // catch 404 and forward to error handler
