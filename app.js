e6cb69d54bd71d1eeadcc4011e3bf6d041c6f962
Anup <anup@comitydesigns.com>
2014-12-03 20:11:42 -0800

    added logic

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -56,7 +57,7 @@ alert("POSTING");
 
 app.put('/',function(req,res){
 
-alert("Putting");
+
 })
 
 // catch 404 and forward to error handler
