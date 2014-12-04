ca1e33806c3e011982a9523b3941c1605acd04e8
Anup <anup@comitydesigns.com>
2014-12-03 20:55:52 -0800

    added logic

--- app.js
+++ app.js
@@ -5,15 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var decode = require('salesforce-signed-request');
-
+var ejs = require('ejs');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'html'));
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -30,8 +29,9 @@ var html_dir = './html/';
 app.use(express.static(path.join(__dirname, 'html')));
 
 app.get('/sf',function(req,res){
-
-    res.sendfile(html_dir + 'index.html');
+    
+    res.render('index');
+    // res.sendfile(html_dir + 'index.ejs');
  // res.sendFile(path.join(__dirname, '../canvas_example', 'index.html'));
 });
 
@@ -69,25 +69,25 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+// if (app.get('env') === 'development') {
+//     app.use(function(err, req, res, next) {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err
+//         });
+//     });
+// }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+// app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: {}
+//     });
+// });
 
 
 module.exports = app;
