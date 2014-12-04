611cca0797156064b66b916dcdeccbddf7f235b1
Anup <anup@comitydesigns.com>
2014-12-03 21:31:34 -0800

    added logic

--- app.js
+++ app.js
@@ -59,11 +59,11 @@ app.put('/',function(req,res){
 })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 
 // error handlers
 
